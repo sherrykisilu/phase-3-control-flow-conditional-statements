0f1781f3d0dde7323c7a5f6f4b0eda6c2de9364e
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-03-14 13:28:49 +0300

    done

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,57 @@
+require 'pry'
 def admin_login(username, password)
-  # your code here
+  if username.upcase == "ADMIN" && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
-
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    return "It's a little chilly out there!"
+  elsif temperature > 85
+    return "It's too dang hot out there!"
+  else
+    return "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 === 0 && num % 5 === 0
+     "FizzBuzz";
+  
+  elsif num % 3 === 0
+     "Fizz";
+
+  elsif num % 5 === 0
+      "Buzz";
+  
+  else 
+    num;
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    if num2 != 0 
+      return num1 / num2
+    else
+      puts "Error: Division by zero!"
+      return nil
+    end
+  else
+    puts "Invalid operation!"
+    return nil
+  end
 end
+
 
